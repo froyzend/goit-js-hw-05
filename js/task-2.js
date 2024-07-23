f73dcbc4dc15f8fcd29fd99b4cdf76d697360fac
Froyzend <froyzend1993@gmail.com>
2024-07-23 03:02:58 +0200

    homework-5

--- js/task-2.js
+++ js/task-2.js
@@ -1,43 +1,45 @@
 'use strict';
 
 
-function calcAverageCalories(days) {
-    let totalCalories = 0;
-    const daysValues = Object.values(days);
-    for (let day of days) {
-        totalCalories += day.calories;
-    }
-    if (totalCalories === 0) {
-        return 0;
-    } else {
-        return totalCalories / daysValues.length;
-    }
-}
+const getUsersWithFriends = (users, friendName) => {
+    return users
+        .filter(user => user.friends.includes(friendName))
+        .map(user => user.name);
+};
 
-console.log(
-    calcAverageCalories([
-        { day: "monday", calories: 3010 },
-        { day: "tuesday", calories: 3200 },
-        { day: "wednesday", calories: 3120 },
-        { day: "thursday", calories: 2900 },
-        { day: "friday", calories: 3450 },
-        { day: "saturday", calories: 3280 },
-        { day: "sunday", calories: 3300 }
-    ])
-);
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
 
-console.log(
-    calcAverageCalories([
-        { day: "monday", calories: 2040 },
-        { day: "tuesday", calories: 2270 },
-        { day: "wednesday", calories: 2420 },
-        { day: "thursday", calories: 1900 },
-        { day: "friday", calories: 2370 },
-        { day: "saturday", calories: 2280 },
-        { day: "sunday", calories: 2610 }
-    ])
-);
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
 
-console.log(
-    calcAverageCalories([])
-);
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross"));
